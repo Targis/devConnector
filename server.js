78827552457f06c4004e5944d67e9723e798f36f
Yuri Kozyk <y.kozyk@gmail.com>
2021-10-16 13:44:57 +0300

    add basic routes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require('express')
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => res.send('API Running'));
+// Connect Database
+connectDB()
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('API Running'))
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/users', require('./routes/api/users'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
