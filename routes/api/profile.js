1b3ea7507a08421cb5ae4d984509d353f4e31095
Yuri Kozyk <y.kozyk@gmail.com>
2021-10-19 21:32:16 +0300

    add Get github repos route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const config = require('config')
+const axios = require('axios')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
@@ -327,6 +329,29 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  Puplic
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    )
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
+    }
+
+    const gitHubResponse = await axios.get(uri, { headers })
+
+    res.json(gitHubResponse.data)
+  } catch (err) {
+    console.error(err.message)
+    return res.status(404).json({ msg: 'No Github profile found' })
   }
 })
 
