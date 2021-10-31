9cd55ebcee93d9d2507fab98a13c183dd253963c
Yuri Kozyk <y.kozyk@gmail.com>
2021-10-31 20:08:15 +0200

    add Profiles component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
+import Profiles from './components/profiles/Profiles'
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import { Provider } from 'react-redux'
@@ -37,8 +38,9 @@ const App = () => {
           <section className="container">
             <Alert />
             <Switch>
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
